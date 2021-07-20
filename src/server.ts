0caca4cc55d1a041637a1d4e8c5747d4cb41e36c
Stefan Dimitrov <stefan@dimitrov.li>
2021-07-19 22:18:02 -0400

    feat: public relays list

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,27 @@
 'use strict';
 if (process.env.NEW_RELIC_LICENSE_KEY) {
-    require('newrelic');
+  require('newrelic');
 }
 import app from './app';
 
 /**
  * Start Express server.
  */
-const server = app.listen(app.get('port'), () => {
-    console.log(
-        '* UHST Relay version %s is running at http://%s:%d in %s mode',
-        app.get('version'),
-        app.get('host'),
-        app.get('port'),
-        app.get('env')
+const server = app.listen(app.get('port'), app.get('host'), () => {
+  console.log(
+    '* UHST Relay version %s is running at http://%s:%d in %s mode',
+    app.get('version'),
+    app.get('host') ?? '127.0.0.1',
+    app.get('port'),
+    app.get('env')
+  );
+  if (app.get('public')) {
+    console.warn(
+      'Running in Public Relay mode. Please ensure the Internet-accessible HTTPS URL of this relay is listed here: %s .',
+      app.get('relays_list')
     );
-    if (app.get('public')) {
-        console.warn('Running in Public Relay mode. Please connect as host (without specifying hostId) to the Internet-accessible URL of this relay over HTTPS and it will be added to the public directory.');
-    }
-    console.log('  Press CTRL-C to stop\n');
+  }
+  console.log('  Press CTRL-C to stop\n');
 });
 
 export default server;
